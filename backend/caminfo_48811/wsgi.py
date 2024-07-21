e8d4e80ebbd6fe93b351d531223ce4deb64fb743
Crowdbotics <team@crowdbotics.com>
2024-07-21 08:07:53 +0000

    Initial commit

---
+++ backend/caminfo_48811/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for caminfo_48811 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "caminfo_48811.settings"
+)
+
+application = get_wsgi_application()
